35b4417262f4b46dce327a3b34122e0ec719ea1e
evelyn-s-hu <evelynhu@ucla.edu>
2022-04-25 00:58:08 -0700

    week 4

--- Week4/js/map.js
+++ Week4/js/map.js
@@ -6,7 +6,7 @@ let zl = 3;
 let markers = L.featureGroup();
 
 // path to csv data
-let path = "data/dunitz.csv";
+let path = "data/casenumbers_apr24.csv";
 
 // initialize
 $( document ).ready(function() {
@@ -42,38 +42,52 @@ function mapCSV(data){
 
     // circle options
         let circleOptions = {
-            radius: 5,
+            radius: 3,
             weight: 1,
             color: 'white',
             fillColor: 'dodgerblue',
             fillOpacity: 1
         }
     
-        // loop through each entry
+        // loop through each entry, skipping null entries
         data.data.forEach(function(item,index){
-            // create a marker
-            let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
-            .on('mouseover',function(){
-                this.bindPopup(`${item.title}<br><img src="${item.thumbnail_url}">`).openPopup()
-            })
-    
-            // add marker to featuregroup
-            markers.addLayer(marker)
-    
-            // add entry to sidebar
-            $('.sidebar').append(`<img src="${item.thumbnail_url}" onmouseover="panToImage(${index})">`)
+            if(typeof item.lat !== 'undefined' && typeof item.lng !== 'undefined'){
+                // create a marker
+                let marker = L.circleMarker([item.lat,item.lng],circleOptions)
+                .on('mouseover',function(){
+                    this.bindPopup(`${item.city_state}<br>Number of hate crimes: ${item.count_crimes}`).openPopup()
+                })
+        
+                // add marker to featuregroup
+                markers.addLayer(marker)
+
+                // add data to sidebar with onclick event
+                $('.sidebar').append(`<div class="sidebar-item" onclick="flyToIndex(${index})">${item.city_state}</div>`)
+
+         }
         })
     
         // add featuregroup to map
         markers.addTo(map)
-    
-        // fit map to markers
-        map.fitBounds(markers.getBounds())
-    }
 
-    function panToImage(index){
-        // zoom to level 17 first
-        map.setZoom(17);
-        // pan to the marker
-        map.panTo(markers.getLayers()[index]._latlng);
     }
+// function to fly to a location by a given id number
+function flyToIndex(index){
+	map.flyTo([data[index].lat,data[index].lng],12)
+}
+
+// define layers
+let layers = {
+	"My Markers": myMarkers
+}
+
+// add layer control box
+L.control.layers(null,layers).addTo(map)
+
+// function to fly to a location by a given id number
+function flyByIndex(index){
+	map.flyTo([data[index].lat,data[index].lon],12)
+
+	// open the popup
+	myMarkers.getLayers()[index].openPopup()
+}
